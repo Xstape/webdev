ee6596ffdfcb45464b15097af592ecba15e56153
Xstape <xstape@MacBook-Pro-Roman.local>
2022-03-04 02:43:19 +0300

    added tabs; js slides

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -24,7 +24,7 @@ const slider = tns({
     controls: false,
     nav: false,
     responsive: {
-        991: {
+        992: {
             autoplay: false,
             nav: false
         }
@@ -37,4 +37,25 @@ document.querySelector('.prev').addEventListener('click', function () {
 
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
+});
+
+$(document).ready(function(){
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault(); // чтобы не переходить по адрессу, например, ссылки, а выполнять какое-либо другое действие
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back')
 });
