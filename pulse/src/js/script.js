ac9c69beb0b9b18a66d412f7363a484d55f05480
Xstape <xstape@mail.ru>
2022-03-08 20:51:45 +0300

    wow animations

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -107,27 +107,29 @@ $(document).ready(function(){
     });
 
     // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+    $("a[href=#up]").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-      // Store hash
-      const hash = this.hash;
+        // Store hash
+        const hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 50, function(){
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 50, function(){ // Remove, because need one more click
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }; // End if
-  });
+            // Add hash (#) to URL when done scrolling (default click behavior)
+        // window.location.hash = hash;
+        });
+        }; // End if
+    });
+
+    new WOW().init();
 });
 
 // const slider = tns({
